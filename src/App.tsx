6dfa4b71339cf1fa1a8df1252bb1554533272271
Denys <Checotun94@gmail.com>
2022-10-13 20:49:03 +0300

    прописали синтаксис вызова еще раз

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,58 @@ import React from 'react'
 
 type TitleProps = {
     title: string
-    text?: string
+    text: string
+    number: string
 }
 
-
-const Title = (props: TitleProps) => <h1>{props.text} {props.title}</h1>
+const Title = (props: TitleProps) => (
+    <>
+    <h1>
+        {props.text} {props.title}
+    </h1>
+    <p>{props.number}</p>
+    </>
+)
 const List = () => {
     let a = 10
     return (
-        <><ul>
-            <li>list 1 {a + 1}</li>
-            <li>list 2</li>
-            <li>list 3</li>
-        </ul>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim id, accusamus deleniti debitis esse eaque iste sunt laudantium deserunt nostrum quis magnam quasi illum animi vitae nobis omnis! Dolores, omnis?</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat hic adipisci ut, atque omnis laudantium alias distinctio, dolorem, doloribus ad quod dignissimos? Quos sapiente necessitatibus voluptates, iusto ut corporis tempora itaque repudiandae blanditiis, aspernatur voluptatem odit sequi exercitationem commodi ea esse expedita? Accusamus temporibus fugiat vero facilis quasi asperiores ipsam, impedit porro doloremque doloribus veniam quod ullam dolore laudantium fugit, ducimus eaque magni hic. Atque suscipit assumenda minima sequi omnis veritatis, consectetur doloremque rerum numquam blanditiis, alias vel neque praesentium corporis, esse debitis! Sint, accusamus. Quas veritatis voluptatibus assumenda corporis impedit minus! Eligendi quod minus dolor at nisi cumque veritatis.
-        </p>
+        <>
+            <ul>
+                <li>list 1 {a + 1}</li>
+                <li>list 2</li>
+                <li>list 3</li>
+            </ul>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                id, accusamus deleniti debitis esse eaque iste sunt laudantium
+                deserunt nostrum quis magnam quasi illum animi vitae nobis
+                omnis! Dolores, omnis?
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+                hic adipisci ut, atque omnis laudantium alias distinctio,
+                dolorem, doloribus ad quod dignissimos? Quos sapiente
+                necessitatibus voluptates, iusto ut corporis tempora itaque
+                repudiandae blanditiis, aspernatur voluptatem odit sequi
+                exercitationem commodi ea esse expedita? Accusamus temporibus
+                fugiat vero facilis quasi asperiores ipsam, impedit porro
+                doloremque doloribus veniam quod ullam dolore laudantium fugit,
+                ducimus eaque magni hic. Atque suscipit assumenda minima sequi
+                omnis veritatis, consectetur doloremque rerum numquam
+                blanditiis, alias vel neque praesentium corporis, esse debitis!
+                Sint, accusamus. Quas veritatis voluptatibus assumenda corporis
+                impedit minus! Eligendi quod minus dolor at nisi cumque
+                veritatis.
+            </p>
         </>
     )
 }
 function App() {
     return (
         <div className="App">
-            <Title title="Test.js"/>  <Title title="App.js"/> <List />
+            <Title title="Test.js" text="Hi" number='10'/>
+            <Title title="App.js" text="hello" number='15'/>
+            <List />
         </div>
     )
 }
