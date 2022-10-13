1048f166ef4859c817bdb76ca102184ff1492fcd
Denys <Checotun94@gmail.com>
2022-10-13 20:06:05 +0300

    преобразовали в код стрелочными функциями

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
+//React Element
 
 //with jsx
-const title = <h1>Hello React js</h1>
+//const title = <h1>Hello React js</h1>
 
 //const title = React.createElement("h1", null, 'React')
 
-const list = (
-    <ul>
-        <li>list 1</li>
-        <li>list 2</li>
-        <li>list 3</li>
-    </ul>
-)
+//React component
+const Title = () => <h1>Hello React component</h1>
+const List = () => {
+    let a = 10
+    return (
+        <ul>
+            <li>list 1 {a + 1}</li>
+            <li>list 2</li>
+            <li>list 3</li>
+        </ul>
+    )
+}
 function App() {
-    return <div className="App">{title} {list} {10+5}</div>
+    return (
+        <div className="App">
+            <Title /> <List /> {10 + 5}
+        </div>
+    )
 }
 
 export default App
