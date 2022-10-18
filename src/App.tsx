3cfed12e9a6c089dfabd5873b269643ac3868238
Denys <Checotun94@gmail.com>
2022-10-18 14:45:53 +0300

    default export и именной

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,23 @@
-import { type } from '@testing-library/user-event/dist/type'
-import React from 'react'
-//React Element
+/*import React from 'react'
 
-//with jsx
-//const title = <h1>Hello React js</h1>
-
-//const title = React.createElement("h1", null, 'React')
-
-//React component
-
-type TitleProps = {
-    title: string
-    text: string
-    number: string
-}
-
-const Title = (props: TitleProps) => (
-    <>
-    <h1>
-        {props.text} {props.title}
-    </h1>
-    <p>{props.number}</p>
-    </>
-)
-const List = () => {
-    let a = 10
+const App = () => {
     return (
         <>
-            <ul>
-                <li>list 1 {a + 1}</li>
-                <li>list 2</li>
-                <li>list 3</li>
-            </ul>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-                id, accusamus deleniti debitis esse eaque iste sunt laudantium
-                deserunt nostrum quis magnam quasi illum animi vitae nobis
-                omnis! Dolores, omnis?
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
-                hic adipisci ut, atque omnis laudantium alias distinctio,
-                dolorem, doloribus ad quod dignissimos? Quos sapiente
-                necessitatibus voluptates, iusto ut corporis tempora itaque
-                repudiandae blanditiis, aspernatur voluptatem odit sequi
-                exercitationem commodi ea esse expedita? Accusamus temporibus
-                fugiat vero facilis quasi asperiores ipsam, impedit porro
-                doloremque doloribus veniam quod ullam dolore laudantium fugit,
-                ducimus eaque magni hic. Atque suscipit assumenda minima sequi
-                omnis veritatis, consectetur doloremque rerum numquam
-                blanditiis, alias vel neque praesentium corporis, esse debitis!
-                Sint, accusamus. Quas veritatis voluptatibus assumenda corporis
-                impedit minus! Eligendi quod minus dolor at nisi cumque
-                veritatis.
-            </p>
+            <h1>Hello App!!!</h1>
         </>
     )
 }
-function App() {
-    return (
-        <div className="App">
-            <Title title="Test.js" text="Hi" number='10'/>
-            <Title title="App.js" text="hello" number='15'/>
-            <List />
-        </div>
-    )
+
+export default App*/
+
+//tsrafce
+
+import React from 'react'
+
+type Props = {}
+
+export const App = (props: Props) => {
+    return <div>App</div>
 }
 
-export default App
+//export default App
