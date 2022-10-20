acee6606c81d8b0b1d847e3d928e45201bcc86ab
Denys <Checotun94@gmail.com>
2022-10-20 18:44:59 +0300

    add Menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -3,10 +3,10 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/system/Container'
+import Menu from 'components/Menu/Menu'
 
 type Props = {}
 
@@ -32,7 +32,7 @@ const Header = (props: Props) => {
                         >
                             Fake shop
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        <Menu />
                     </Toolbar>
                 </Container>
             </AppBar>
