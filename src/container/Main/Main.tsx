010d89386dffd92e70c7080c555ac4be2baee41e
Denys <Checotun94@gmail.com>
2022-11-07 15:38:47 +0200

    add react-router

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Container } from "@mui/material"
 import Home from "pages/Home/Home"
+import { Routes, Route } from "react-router-dom"
+import CartPage from "pages/Cart/CartPage"
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
@@ -10,7 +12,13 @@ const Main = ({ addProductToCart }: Props) => {
     return (
         <main>
             <Container maxWidth="lg">
-                <Home addProductToCart={addProductToCart} />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="cart" element={<CartPage />} />
+                </Routes>
             </Container>
         </main>
     )
